d8905f84d43a0280dde93c3c3bd0dc061e247ccc
vividkim4228 <vividkim4228@gmail.com>
2019-05-13 21:48:55 +0900

    edit

--- js/src/sun_portfolio.js
+++ js/src/sun_portfolio.js
@@ -26,7 +26,7 @@
 		if(thisIndex==0){
 			pic.attr('class','pic gillette');
 			text.attr('class','text gillette');
-			textDt.css({'background-image':'url(../../img/portfolio/logo_gillette_white.svg)','backgroundSize':'auto 70%'});
+			textDt.css({'background-image':'url(../img/portfolio/logo_gillette_white.svg)','backgroundSize':'auto 70%'});
 			textDd.eq(0).text('질레트 코리아 [gillette korea]');
 			textDd.eq(1).text('질레트 면도기 및 면도용품 판매 및 제품소개를 위한 웹사이트입니다.');
 			link.attr('href','https://vividkimweb.github.io/gillette')
@@ -35,7 +35,7 @@
 		}else if(thisIndex==1){
 			pic.attr('class','pic kellogg');
 			text.attr('class','text kellogg');
-			textDt.css({'background-image':'url(../../img/portfolio/logo_kellogg_white.svg','backgroundSize':'auto 90%'});
+			textDt.css({'background-image':'url(../img/portfolio/logo_kellogg_white.svg','backgroundSize':'auto 90%'});
 			textDd.eq(0).text('농심 켈로그 [kellogg]');
 			textDd.eq(1).text('켈로그 시리얼 제품 제품소개 및 마케팅을 위한 웹사이트입니다.');
 			link.attr('href','https://vividkimweb.github.io/kellogg')
