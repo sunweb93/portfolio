67a49b8d5792ef0d0aaa93b9901a4d83f84858fc
vividkim4228 <vividkim4228@gmail.com>
2019-05-26 22:19:57 +0900

    renew

--- js/src/common/sun_header.js
+++ js/src/common/sun_header.js
@@ -10,4 +10,52 @@
 	});
 
 
+
+var gnb        = $('.gnb');
+var gnbUl      = gnb.children('ul');
+var gnbLi      = gnbUl.children('li');
+var gnbBtn     = gnbLi.children('button');
+var aboutBox   = $('.about_box');
+var webBox     = $('.web_box');
+var otherBox   = $('.other_box');
+var contactBox = $('.contact_box');
+
+// header_click_scroll
+gnbBtn.on('click',function(e){
+	var aboutTop   = aboutBox.offset().top;
+	var webTop     = webBox.offset().top;
+	var otherTop   = otherBox.offset().top;
+	var contactTop = contactBox.offset().top;
+	var btnI = $(this).parent('li').index();
+	
+	e.preventDefault();
+	if(btnI == 0){
+		$('html').animate({scrollTop: aboutTop - '100'}, 500);
+	}else if(btnI == 1){
+		$('html').animate({scrollTop: webTop - '100'}, 500);
+	}else if(btnI == 2){
+		$('html').animate({scrollTop: otherTop - '100'}, 500);
+	}else if(btnI == 3){
+		$('html').animate({scrollTop: contactTop - '100'}, 500);
+	};
+
+});
+
+
+// top button
+var topBtn = $('.top_btn');
+
+$(window).scroll(function(){
+  if($(this).scrollTop() > 500) {
+      topBtn.addClass('show');
+  } else {
+			topBtn.removeClass('show');
+  }
+});
+
+topBtn.on('click',function(e){
+	e.preventDefault();
+	$('html').animate({scrollTop: '0'}, 500);
+})
+
 })(jQuery);
--- js/src/sun_main.js
+++ js/src/sun_main.js
@@ -8,4 +8,33 @@
 	headBox.load('./common/sun_header.html');
 
 
+	var otherBox = $('.other_ul');
+	var otherLi  = otherBox.children('li');
+	var modalBox = $('.modal');
+	var modalPic = $('.modal_pic');
+
+	otherLi.on('click',function(e){
+		var thisI = $(this).index();
+
+		e.preventDefault();
+		modalPic.css({
+			'background-size' : 'contain', 
+			'background-repeat' : 'no-repeat', 
+			'background-position':'center'});
+		if(thisI == 0){
+			modalPic.css({
+			'backgroundImage':'url(../../../img/main/umbro_banner_01.png)'});
+		}else if(thisI ==1){
+			modalPic.css({
+			'backgroundImage':'url(../../../img/main/umbro_banner_02.png)'});
+		}
+		modalBox.fadeIn();
+	});
+
+	modalBox.on('click',function(e){
+		e.preventDefault();
+		$(this).fadeOut();
+	})
+
+
 })(jQuery);
